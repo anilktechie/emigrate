0179a31c2f384c94b7eae8613e6b73f7a251c20f
Vitold S <vit1251@gmail.com>
2014-08-19 03:56:19 +0400

    Directory review

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+from setuptools import setup
+
+setup(
+    name = "emigrate",
+    version = "0.1",
+    packages = ['emigrate'],
+    package_dir = {'': 'src'},
+    entry_points = {
+        'console_scripts': [
+            'emig = emigrate.app:main',
+        ],
+    },
+    install_requires=[
+        'twisted',
+    ],
+);
