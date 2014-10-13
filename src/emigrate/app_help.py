e7ef480bf260b561eeffbd6dd6056b9a5b0ec38d
Vitold S <vit1251@gmail.com>
2014-10-13 14:59:18 +0400

    Update app_help.py

--- src/emigrate/app_help.py
+++ src/emigrate/app_help.py
@@ -5,20 +5,39 @@ from app_base import ApplicationCommand
 
 
 class ApplicationCommandHelp(ApplicationCommand):
-    def run(self):
-        content = "Usage: %s [command]\n" % (sys.argv[0], )
-        content += "Emigrate command-line client, version %s.\n" % ("0.1", )    # TODO - how to get that version form __init__
-        content += "\n"
-        content += "The most commonly used commands are:\n"
-        content += "\n"
-        content += "    create    - create migration in .migration directory\n"
-        content += "    up        - upgrade migration operation\n"
-        content += "    down      - downgrade migration operation\n"
-        content += "    redo      - redo migration operation (when previous is with exception)\n"
-        content += "    history   - show migration operation\n"
-        content += "    new       - show new migration\n"
-        content += "\n"
-        content += "Emigrate is a tool for database schema version control.\n"
-        content += "For additional information, see http://www.github.com/vit1251/emigrate\n"
+    
+    def __get_version(self, default="UNKNOWN"):
+        result = default
         #
+        app = self.getApplication()
+        if hasattr(app, "VERSION"):
+            result = app.VERSION
+        #
+        return result
+    
+    def __create_help_message(self):
+        """ Create help message
+        """
+        items = []
+        items.append("Usage: {cmd} [command]".format(cmd=sys.argv[0]))
+        items.append("Emigrate command-line client, version {version}.".format(version=self.__get_version()))
+        items.append("")
+        items.append("The most commonly used commands are:")
+        items.append("")
+        items.append("    create    - create migration in .migration directory")
+        items.append("    up        - upgrade migration operation")
+        items.append("    down      - downgrade migration operation")
+        items.append("    redo      - redo migration operation (when previous is with exception)")
+        items.append("    history   - show migration operation")
+        items.append("    new       - show new migration")
+        items.append("")
+        items.append("Emigrate is a tool for database schema version control.")
+        items.append("For additional information, see http://www.github.com/vit1251/emigrate")
+        content = "\n".join(items)
+        return content
+    
+    def run(self):
+        """ Main entry point
+        """
+        help_message = self.__create_help_message()
         sys.stdout.write(content)
